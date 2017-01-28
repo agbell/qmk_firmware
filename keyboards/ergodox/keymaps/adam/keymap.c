1066e4dce92f89515d6841ffde2bdcb481193720
Adam Bell <agbell@gmail.com>
2017-01-28 13:47:19 -0500

    adding macro for shifted tap

--- keyboards/ergodox/keymaps/adam/keymap.c
+++ keyboards/ergodox/keymaps/adam/keymap.c
@@ -4,42 +4,44 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define FLOCK 1 // symbols
-#define JLOCK 2 // media keys
+#define FLOCK 1 // symbols arrows and F keys on F held down
+#define JLOCK 2 // same as Flock but with fall thru J and mapped to J held down
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+// Sends macro when key is tapped, presses mod when key is held
+#define tap_mod_macro(record, mod, macro) ( ((record)->event.pressed) ? \
+     ( ((record)->tap.count <= 0 || (record)->tap.interrupted) ? MACRO(D(mod), END) : MACRO_NONE ) : \
+     ( ((record)->tap.count > 0 && !((record)->tap.interrupted)) ? (macro) : MACRO(U(mod), END) ) )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Layout
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |        |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * |        |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |;     |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |       |     |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        |      |      |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      |------|       |------|        |      |
+ *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
-// If it accepts an argument (i.e, is a function), it doesn't need KC_.
-// Otherwise, it needs KC_*
         [BASE] = KEYMAP(
                 // left hand
                 GUI_T(KC_ESC), KC_1, KC_2,   KC_3, KC_4, KC_5, KC_MINS,
                 KC_LBRC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_TAB,
-                KC_LCBR, SFT_T(KC_A), ALT_T(KC_S), CTL_T(KC_D), FUNC(1), GUI_T(KC_G),
+                F(3), SFT_T(KC_A), ALT_T(KC_S), CTL_T(KC_D), F(1), GUI_T(KC_G),
                 KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BSPC,
                 KC_BSLS, KC_GRAVE,  _______,  _______ ,_______,
                 _______,  _______,
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 // right hand
                 KC_EQL,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             _______,
                 KC_TAB,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_RBRC,
-                GUI_T(KC_H),   FUNC(2),   CTL_T(KC_K),   ALT_T(KC_L),   SFT_T(KC_SCLN),           KC_RCBR,
+                GUI_T(KC_H),   F(2),   CTL_T(KC_K),   ALT_T(KC_L),   SFT_T(KC_SCLN),           F(4),
                 KC_DELETE,  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_QUOT,           KC_LSHIFT,
                 _______,  _______,_______,_______,          KC_SLSH,
                 _______,        _______,
@@ -76,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 _______,
                 _______, _______, _______
         ),
-        [FLOCK] = KEYMAP(
+        [JLOCK] = KEYMAP(
                 // left hand
                 _______,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
                 _______,KC_LBRC,KC_NUHS,  KC_TILD,_______,_______,_______,
@@ -100,7 +102,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
         [1] = ACTION_LAYER_TAP_KEY(FLOCK,KC_F),
-        [2] = ACTION_LAYER_TAP_KEY(JLOCK,KC_J)            // FN1 - Momentary Layer 1 (Symbols)
+        [2] = ACTION_LAYER_TAP_KEY(JLOCK,KC_J),
+        [3] = ACTION_MACRO_TAP(3),
+        [4] = ACTION_MACRO_TAP(4)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -112,7 +116,16 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                         SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
                 }
                 break;
+        case 3:
+            // the special Alt / { macro
+            return tap_mod_macro(record, LALT, MACRO( D(LSFT), T(LBRC), U(LSFT), END));
+            break;
+       case 4:
+            // the special Alt / } macro
+            return tap_mod_macro(record, LALT, MACRO( D(LSFT), T(RBRC), U(LSFT), END));
+            break;
         }
+
         return MACRO_NONE;
 };
 
@@ -123,24 +136,5 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
-        uint8_t layer = biton32(layer_state);
-
-        ergodox_board_led_off();
-        ergodox_right_led_1_off();
-        ergodox_right_led_2_off();
-        ergodox_right_led_3_off();
-        switch (layer) {
-        // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
-                ergodox_right_led_1_on();
-                break;
-        case 2:
-                ergodox_right_led_2_on();
-                break;
-        default:
-                // none
-                break;
-        }
 
 };
