e7a7122acbb98aad5e513ea1e3359431238acdf4
Adam Bell <agbell@gmail.com>
2017-05-31 20:45:49 -0400

    mapping stuff

--- keyboards/lets_split/keymaps/adam/config.h
+++ keyboards/lets_split/keymaps/adam/config.h
@@ -37,6 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 300 //At 500 some bad logic takes hold
+#define TAPPING_TERM 200 //At 500 some bad logic takes hold
 #define PREVENT_STUCK_MODIFIERS
 #define IGNORE_MOD_TAP_INTERRUPT
